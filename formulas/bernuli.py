f3cca25335955e3791cc07f8e2158f4cf7856bcf
Matveev Stepan Andreevich <mset321@gmail.com>
2024-03-27 20:41:12 +0300

    fix formulas

--- formulas/bernuli.py
+++ formulas/bernuli.py
@@ -1,5 +1,5 @@
 from math import factorial as fact
-from operations_enum import OperationsEnum
+from inequality_operations import InequalityOperations
 
 
 class BernuliCalcul:
@@ -15,27 +15,26 @@ class BernuliCalcul:
     def bernuli_in_range(self, n: int, m1: int, m2: int, p: float) -> float:
         return sum(self.bernuli(n, m, p) for m in range(m1, m2+1))
 
-    def bernuli_calcul(self, n: int, m1: int, m2: int, p: float, operation: OperationsEnum) -> float:
+    def bernuli_calcul(self, n: int, m1: int, m2: int, p: float, operation: InequalityOperations) -> float:
         match operation:
-            case OperationsEnum.equal:
+            case InequalityOperations.equal:
                 return self.bernuli(n, m1, p)
-            case OperationsEnum.less:
-                return self.bernuli_in_range(n, m1-1, 0, p)
-            case OperationsEnum.more_or_equal:
-                return 1 - self.bernuli_in_range(n, m1-1, 0, p)
-            case OperationsEnum.between:
+            case InequalityOperations.less:
+                return self.bernuli_in_range(n, 0, m1-1, p)
+            case InequalityOperations.more_or_equal:
+                return self.bernuli_in_range(n, m1, n, p)
+            case InequalityOperations.between:
                 return self.bernuli_in_range(n, m1, m2, p)
 
-    def bernuli_polynomial(self, m: list[int], p: list[int]) -> float:
+    def bernuli_polynomial(self, m: list[int], p: list[float]) -> float:
         n = sum(m)
 
-        mult1 = 0
+        mult1 = 1
         for i in m:
             mult1 *= fact(i)
 
-        mult2 = 0
+        mult2 = 1
         for i, j in zip(m, p):
             mult2 *= j**i
 
         return fact(n) / mult1 * mult2
-
--- formulas/integral_laplace.py
+++ formulas/integral_laplace.py
@@ -8,11 +8,11 @@ class IntegralLaplace:
         return term * t / (2 * n + 1)
 
     def integral(self, x1, x2) -> float:
-        integral1 = sum(self.integral_term(n, x1) for n in range(150))
-        integral2 = sum(self.integral_term(n, x2) for n in range(150))
+        integral1 = sum(self.integral_term(n, x1) for n in range(70))
+        integral2 = sum(self.integral_term(n, x2) for n in range(70))
         return integral1 - integral2
 
-    def laplace(self, n: int, m1: int, m2: int, x: int, p: float) -> float:
+    def laplace_integer(self, n: int, m1: int, m2: int, p: float) -> float:
         q = 1 - p
 
         x1 = (m1 - n*p) / (n * p * q)**0.5
